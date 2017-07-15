615c0e8c1500a626b6e183233805a39daf741032
Rick <rickwillcoxau@outlook.com>
2017-07-15 21:10:40 +1000

    first commit

--- ContactsApp/MainPage.xaml.cs
+++ ContactsApp/MainPage.xaml.cs
@@ -19,54 +19,142 @@ using System.Xml.Linq;
 using Windows.Storage;
 using System.Collections.ObjectModel;
 using System.Reflection;
+using System.Diagnostics;
+using Windows.ApplicationModel;
+using Windows.Storage.Streams;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ContactsApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
 
+        //string XMLPath = Path.Combine(Package.Current.InstalledLocation.Path, "Contacts.xml");
         private ObservableCollection<string> lstd = new ObservableCollection<string>();
         public ObservableCollection<string> myList { get { return lstd; } }
         public MainPage()
         {
             this.InitializeComponent();
-
         }
 
-        private void Grid_Loading(FrameworkElement sender, object args)
+        private async void Grid_Loading(FrameworkElement sender, object args)
         {
             loadContacts();
         }
-        public void loadContacts()
+        public async void loadContacts()
         {
-            const string fileName = "Contacts.xml";
 
-            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-
-            var path = assembly.GetManifestResourceNames()
-                .FirstOrDefault(n => n.EndsWith(fileName, StringComparison.OrdinalIgnoreCase));
-
-            if (path == null)
-                throw new Exception("File not found");
-
-            using (var stream = assembly.GetManifestResourceStream(path))
-            using (var reader = XmlReader.Create(stream))
+            StorageFile file = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("Contacts.xml");
+            XmlReader xmlReader = XmlReader.Create(file.Path);
+            while (xmlReader.Read())
             {
-                while (reader.Read())
+                if (xmlReader.Name.Equals("ID") && (xmlReader.NodeType == XmlNodeType.Element))
                 {
-                    if (reader.Name.Equals("FirstName") && (reader.NodeType == XmlNodeType.Element))
-                    {
-                        lstd.Add(reader.ReadElementContentAsString());
-                    }
+                    lstd.Add(xmlReader.ReadElementContentAsString());
                 }
             }
+            DataContext = this;
+            xmlReader.Dispose();
+        }
 
-            DataContext = this; // better to move this inside the constructor
+        private async void lstBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            StorageFile file = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("Contacts.xml");
+            try
+            {
+                XDocument xml = XDocument.Load(file.Path);
+                if (lstBox.SelectedIndex != -1)
+                {
+                    var nodes = (from n in xml.Descendants("Contact").
+                Where(r => r.Element("ID").Value == lstBox.SelectedItem.ToString())
+                                 select new
+                                 {
+                                     ID = (string)n.Element("ID").Value,
+                                     FirstName = (string)n.Element("FirstName").Value,
+                                     LastName = (string)n.Element("LastName").Value,
+                                     Mobile = (string)n.Element("Mobile").Value,
+                                     Email = (string)n.Element("Email").Value
+                                 });
+                    foreach (var n in nodes)
+                    {
+                        txtID.Text = n.ID;
+                        txtFirstName.Text = n.FirstName;
+                        txtLastName.Text = n.LastName;
+                        txtMobile.Text = n.Mobile;
+                        txtEmail.Text = n.Email;
+                    };
+                }
+                else
+                {
+                    txtID.Text = "";
+                    txtFirstName.Text = "";
+                    txtLastName.Text = "";
+                    txtMobile.Text = "";
+                    txtEmail.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private async void updateXMLFile(XDocument xdoc)
+        {
+            try
+            {
+                //StorageFile file = await installedLocation.CreateFileAsync("Contacts.xml", CreationCollisionOption.ReplaceExisting);
+                StorageFile file = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("Contacts.xml"); //This line was the replacement for the one above.
+                await FileIO.WriteTextAsync(file, xdoc.ToString());
+            }
+            catch (Exception ex)
+            {
+                String s = ex.ToString();
+            }
+        }
+
+        private async void btnUpdateContact_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFile file = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("Contacts.xml");
+            XDocument xdoc = XDocument.Load(file.Path);
+            if (lstBox.SelectedIndex != -1)
+            {
+                XElement upd = (from elements in xdoc.Descendants("Contact")
+                                where elements.Element("ID").Value == lstBox.SelectedItem.ToString()
+                                select elements).Single();
+                upd.Element("ID").Value = txtID.Text;
+                upd.Element("FirstName").Value = txtFirstName.Text;
+                upd.Element("LastName").Value = txtLastName.Text;
+                upd.Element("Mobile").Value = txtMobile.Text;
+                upd.Element("Email").Value = txtEmail.Text;
+                updateXMLFile(xdoc);
+            }
+        }
+
+
+        private async void btnDeleteContact_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFile file = await Windows.Storage.ApplicationData.Current.LocalFolder.GetFileAsync("Contacts.xml");
+            XDocument xdoc = XDocument.Load(file.Path);
+            if (lstBox.SelectedIndex != -1)
+            {
+                xdoc.Element("Contacts")
+                    .Elements("Contact")
+                    .Where(x => (string)x.Element("ID") == lstBox.SelectedItem.ToString()).Remove();
+                lstBox.SelectedIndex = -1; 
+                updateXMLFile(xdoc);
+
+                myList.Clear();
+                
+                loadContacts();
+            }
         }
     }
 }
+
+
+
+
+
+
